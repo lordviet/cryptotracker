708be89fc2586d23f316c971415b172c465957ae
lordviet <viet1231@abv.bg>
2020-06-08 23:49:49 +0300

    coin container upper row styling

--- src/components/Crypto.jsx
+++ src/components/Crypto.jsx
@@ -4,8 +4,11 @@ import { Text, View, StyleSheet } from 'react-native';
 export default function Crypto({ name, symbol, priceUsd, percentChange24h, percentChange7d }) {
     return (
         <View>
-            <Text>{symbol}</Text>
-            <Text>{name}</Text>
+            <View style={coinInfoRow}>
+                <Text style={coinSymbol}>{symbol}</Text>
+                <Text style={seperator}>|</Text>
+                <Text style={coinName}>{name}</Text>
+            </View>
             <Text>{priceUsd}</Text>
             <Text>{percentChange24h} {percentChange7d}</Text>
         </View>
@@ -13,8 +16,27 @@ export default function Crypto({ name, symbol, priceUsd, percentChange24h, perce
 }
 
 const styles = StyleSheet.create({
-    coinName: {},
-    coinSymbol: {},
-    coinPrice:{},
-   // Percent change + green and - red 
+    coinInfoRow:{
+        display: "flex",
+        flexDirection: "row",
+        marginBottom: 15
+    },
+    coinSymbol: {
+        marginTop: 10,
+        marginLeft: 5,
+        marginRight: 5,
+        fontWeight: "bold",
+    },
+    seperator: {
+        marginTop: 10,
+    },
+    coinName: {
+        marginTop: 10,
+        marginLeft: 5,
+        marginRight: 5
+    },
+    coinPrice: {},
+    // Percent change + green and - red 
 });
+
+const { coinInfoRow, coinSymbol, seperator, coinName, coinPrice } = styles;
